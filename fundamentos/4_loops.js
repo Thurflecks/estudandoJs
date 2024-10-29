da8e80dce885f4b47df2d7d1d72d6fa4197a8424
Thurflecks <thurflecks.dev@gmail.com>
2024-10-29 15:45:46 -0300

    loops

---
+++ fundamentos/4_loops.js
@@ -0,0 +1,32 @@
+// for -> mais comun
+// for (incremeto, condição / incre.){}
+
+//incremendo = i, j k ....
+//condiçao = ex.: i<10 
+//incrementador = i ++
+
+//loop de 0 ate 4
+
+for(let i = 0; i<5; i++){
+    console.log('o i é: ', i)
+}
+
+//arrays = listas
+// itens com mesmo dados
+
+const frutas = ['maça', 'banana', 'uva', 'laranja']
+
+console.log(frutas)
+
+// for(let i = 0; i < frutas.length; i++){
+//     console.log(frutas[i])
+// }
+//while 
+let p = 0
+
+while(p < frutas.length){
+    console.log(frutas[p])
+    p++
+}
+
+//
